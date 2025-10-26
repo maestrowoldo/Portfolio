e8f601c404e0cac19b20d447e2e75881e43211d7
maestrowoldo <maestrowoldo97@gmai.com>
2025-10-26 14:33:11 -0300

    Reestrutura projeto

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname )));
 
-app.get('/', (req, res) => {
+app.get('index.html', (req, res) => {
     res.sendFile(path.join(__dirname, 'portfolio', 'index.html'));
 });
 
